91e2fc0f821426e189a54a6139723d45682c105d
viguime <jvictormguimaraes@gmail.com>
2021-07-29 16:46:07 -0300

    Users controller set up

--- src/main/java/com/viguime/Template/API/REST/UsersController.java
+++ src/main/java/com/viguime/Template/API/REST/UsersController.java
@@ -1,4 +1,28 @@
 package com.viguime.Template.API.REST;
 
-public class usersController {
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping
+    public String getUsers() {
+        return "HTTP GET request was sent";
+    }
+
+    @PostMapping
+    public String createUsers() {
+        return "HTTP POST request was sent";
+    }
+
+    @DeleteMapping
+    public String deleteUsers() {
+        return "HTTP Delete request was sent";
+    }
+
+    @PutMapping
+    public String putUsers() {
+        return "HTTP Delete request was sent";
+    }
 }
